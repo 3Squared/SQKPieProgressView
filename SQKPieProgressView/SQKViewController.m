1942a811b0c81f12603eafab9b0baab0afd24b77
Luke Stringer <luke.stringer@3squared.com>
2013-12-02 14:35:04 +0000

    Added SQKPieProgressView and dependencies.

--- SQKPieProgressView/SQKViewController.m
+++ SQKPieProgressView/SQKViewController.m
@@ -7,9 +7,10 @@
 //
 
 #import "SQKViewController.h"
+#import "SQKPieProgressView.h"
 
 @interface SQKViewController ()
-
+@property (nonatomic, strong) SQKPieProgressView *pieProgressView ;
 @end
 
 @implementation SQKViewController
@@ -17,11 +18,37 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    SQKPieProgressView *pieProgressView = [[SQKPieProgressView alloc] init];
+    
+    CGFloat width = 60.0f;
+    CGFloat height = 60.0f;
+    CGFloat xPosition = (CGRectGetWidth(self.view.frame) / 2) - (width / 2);
+    CGFloat yPosition = (CGRectGetHeight(self.view.frame) / 2) - (height / 2);
+    pieProgressView.frame = CGRectMake(xPosition, yPosition, width, height);
+    
+    
+    self.pieProgressView = pieProgressView;
+    [self.view addSubview:self.pieProgressView];
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    [self incrementPieProgress];
+}
+
+- (void)incrementPieProgress {
+    CGFloat newProgress = self.pieProgressView.progress + 0.01;
+    [self.pieProgressView setProgress:newProgress];
+    
+    if (newProgress < 1.0) {
+        [self performSelector:@selector(incrementPieProgress) withObject:nil afterDelay:0.1];
+    }
+}
+
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
