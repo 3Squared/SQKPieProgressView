0ed6866823b3ac750c8728696b18d1797376dff2
Luke Stringer <luke.stringer@3squared.com>
2013-12-02 13:59:20 +0000

    Xcode project.

---
+++ SQKPieProgressView/SQKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SQKAppDelegate.h
+//  SQKPieProgressView
+//
+//  Created by Luke Stringer on 02/12/2013.
+//  Copyright (c) 2013 3Squared. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SQKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SQKPieProgressView/SQKViewController.m
@@ -0,0 +1,29 @@
+//
+//  SQKViewController.m
+//  SQKPieProgressView
+//
+//  Created by Luke Stringer on 02/12/2013.
+//  Copyright (c) 2013 3Squared. All rights reserved.
+//
+
+#import "SQKViewController.h"
+
+@interface SQKViewController ()
+
+@end
+
+@implementation SQKViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
